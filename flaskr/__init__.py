9b9b1dcd9c5c425f1b3d6e232b0fca2a44b439ca
root <root@localhost.localdomain>
2021-02-23 10:44:58 +0800

    first commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -22,10 +22,11 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello,world!'
-    
-    return app
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
